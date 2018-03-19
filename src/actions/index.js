776e5f9cc1dbd5f3caffe64325213e1ea46aadaa
Luke Wendling <luke@lukewendling.com>
2018-03-19 14:12:50 -0500

    add random quote todo from async source

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,47 @@
-let nextTodoId = 0
+let nextTodoId = 0;
 export const addTodo = text => ({
   type: 'ADD_TODO',
   id: nextTodoId++,
   text
-})
+});
 
 export const setVisibilityFilter = filter => ({
   type: 'SET_VISIBILITY_FILTER',
   filter
-})
+});
 
 export const toggleTodo = id => ({
   type: 'TOGGLE_TODO',
   id
-})
+});
+
+export const requestRandom = category => ({
+  type: 'REQUEST_RANDOM',
+  category
+});
+
+export const receiveRandom = json => ({
+  type: 'RECEIVE_RANDOM',
+  json
+});
 
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+
+export function fetchRandom(category) {
+  return dispatch => {
+    dispatch(requestRandom(category));
+    return fetch(`http://quotes.rest/quote/random.json`, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(json => {
+        const { author, quote } = json.contents.quotes[0];
+        dispatch(receiveRandom(json));
+        dispatch(addTodo(`${quote} -${author}`));
+      });
+  };
 }
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
+import GetRandom from '../containers/GetRandom';
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
+    <br />
+    <GetRandom />
   </div>
-)
+);
 
-export default App
+export default App;
